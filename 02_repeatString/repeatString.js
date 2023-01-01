bc5b99724af2c1d7b30874f782db33532fb91305
Rhys Miller <98274805+rjmills87@users.noreply.github.com>
2023-01-01 18:24:04 +0000

    Completed exercises 1-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let repeatedString = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+  while (num > 0) {
+    repeatedString += str;
+    num--;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
