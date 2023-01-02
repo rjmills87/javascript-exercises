a9e1bbb16872cb7633c786e8e4b085581a21d960
Rhys Miller <98274805+rjmills87@users.noreply.github.com>
2023-01-02 19:20:42 +0000

    Completed javascript exercises 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+function sumAll(min, max) {
+  let arr = [min, max].sort((a, b) => a - b);
+  let rangeArr = [];
+  let sum = 0;
 
-};
-
+  for (let i = arr[0]; i < arr[1] + 1; i++) {
+    rangeArr.push(i);
+    sum = rangeArr.reduce((acc, cur) => acc + cur, 0);
+  }
+  if (min < 0 || max < 0 || Number(min) !== min || Number(max) !== max) {
+    return "ERROR";
+  } else {
+    return sum;
+  }
+}
 // Do not edit below this line
 module.exports = sumAll;
