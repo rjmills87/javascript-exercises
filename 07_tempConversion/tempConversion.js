4702618cae9a9c533a27a0b6bc3c1ee383f081d1
Rhys Miller <98274805+rjmills87@users.noreply.github.com>
2023-01-03 12:27:38 +0000

    Completed javascript exercise 07 tempConversion.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (far) {
+  const tempCelsius = (far - 32) * (5 / 9);
+  return Number(tempCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cel) {
+  const tempFahrenheit = cel * 1.8 + 32;
+  return Number(tempFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
