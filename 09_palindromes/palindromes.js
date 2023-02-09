5e0f0749cef5b851213c934423f9c2560460635c
Rhys Miller <rjmills87@aol.com>
2023-02-09 16:27:22 +0000

    Completed palindromes.js exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let reverseString = str
+    .split("")
+    .reverse()
+    .join("")
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/\s{2,}/g, " ")
+    .split(" ")
+    .join("")
+    .toLowerCase();
 
+  let normalSring = str
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/\s{2,}/g, " ")
+    .split(" ")
+    .join("")
+    .toLowerCase();
+
+  if (reverseString === normalSring) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
