a6ee60f84b232e9006b70e97912b1a568b11ad5a
Rhys Miller <rjmills87@aol.com>
2023-02-24 21:19:37 +0000

    Completed 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const date = new Date();
+  const year = date.getFullYear();
 
+  people.forEach((person) => {
+    const isDead = person.hasOwnProperty("yearOfDeath");
+    if (isDead) {
+      person.age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+      person.age = year - person.yearOfBirth;
+    }
+  });
+  people.sort((a, b) => a.age - b.age);
+  const theOldest = people[2];
+  return theOldest;
 };
 
 // Do not edit below this line
